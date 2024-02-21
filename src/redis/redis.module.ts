49683771f2b2fa4ee66a3ba9a63e0196b48841f4
carpenterzoe <carpenterzoe@outlook.com>
2024-02-21 21:22:44 +0800

    feat: 配置都改成ConfigService读env

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -12,21 +12,12 @@ import { ConfigService } from '@nestjs/config';
     {
       provide: 'REDIS_CLIENT',
       async useFactory(configService: ConfigService) {
-        console.log(
-          'redis_server_host',
-          configService.get('redis_server_host'),
-        );
-        console.log(
-          'redis_server_port',
-          configService.get('redis_server_port'),
-        );
-
         const client = createClient({
           socket: {
-            host: 'localhost',
-            port: 6379,
+            host: configService.get('redis_server_host'),
+            port: configService.get('redis_server_port'),
           },
-          database: 1,
+          database: configService.get('redis_server_db'),
         });
         await client.connect();
         return client;
